922eba5ecea4e5db01899d15ace62c23f97da95b
Zsuró Tibor <zsurotibor@gmail.com>
2025-07-18 16:54:43 +0200

    Update kotlin to 2.1.21

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -7,9 +7,9 @@
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.9.0"
+    id("org.jetbrains.kotlin.jvm") version "2.1.21"
 
-    kotlin("kapt") version "1.9.0"
+    kotlin("kapt") version "2.1.21"
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
